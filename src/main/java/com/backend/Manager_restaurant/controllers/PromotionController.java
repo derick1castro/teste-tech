ed250637872a5056df2aa00f0810210781b9145b
Derick <derick1castro@gmail.com>
2023-09-22 09:35:02 -0300

    Documentando Endpoint Promotion

--- src/main/java/com/backend/Manager_restaurant/controllers/PromotionController.java
+++ src/main/java/com/backend/Manager_restaurant/controllers/PromotionController.java
@@ -6,8 +6,15 @@ import com.backend.Manager_restaurant.exceptions.CategoryNotFoundException;
 import com.backend.Manager_restaurant.exceptions.PromotionNotFoundException;
 import com.backend.Manager_restaurant.models.Product;
 import com.backend.Manager_restaurant.models.Promotion;
+import com.backend.Manager_restaurant.models.Restaurant;
 import com.backend.Manager_restaurant.services.ProductService;
 import com.backend.Manager_restaurant.services.PromotionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,12 +27,27 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/v1/promotion")
+@Tag(name = "Promotion", description = "Endpoint for Managing Promotions")
 public class PromotionController {
 
     @Autowired
     private PromotionService promotionService;
 
     @PostMapping
+    @Operation(summary = "Adds a new Promotion",
+            description = "Adds a new Promotion",
+            tags = {"Promotion"},
+            method = "POST",
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content =
+                            @Content(schema = @Schema(implementation = Restaurant.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Promotion> savePromotion(@RequestBody @Valid PromotionRecordDto promotionRecordDto){
         var promotion = new Promotion();
         BeanUtils.copyProperties(promotionRecordDto, promotion);
@@ -33,17 +55,66 @@ public class PromotionController {
     }
 
     @GetMapping
+    @Operation(summary = "Finds all Promotions",
+            description = "Finds all Promotions",
+            tags = {"Promotion"},
+            method = "GET",
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            array = @ArraySchema(schema = @Schema(implementation = Restaurant.class))
+                                    )
+                            }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<List<Promotion>> getAllPromotions(){
         return ResponseEntity.status(HttpStatus.OK).body(promotionService.findAll());
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Finds a Promotion",
+            description = "Finds a Promotion",
+            tags = {"Promotion"},
+            method = "GET",
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200",
+                            content =
+                            @Content(schema = @Schema(implementation = Restaurant.class))
+                    ),
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Object> getOnePromotion(@PathVariable(value = "id") Long id){
         var promotion = promotionService.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(promotion);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Updates a Promotion",
+            description = "Updates a Promotion",
+            tags = {"Promotion"},
+            method = "PUT",
+            responses = {
+                    @ApiResponse(description = "Updated", responseCode = "200",
+                            content =
+                            @Content(schema = @Schema(implementation = Restaurant.class))
+                    ),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Object> updatePromotion(@PathVariable(value = "id") Long id,
                                                    @RequestBody @Valid PromotionRecordDto promotionRecordDto) {
         var oldPromotion = promotionService.findById(id);
@@ -53,6 +124,18 @@ public class PromotionController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Deletes a Promotion",
+            description = "Deletes a Promotion",
+            tags = {"Promotion"},
+            method = "PUT",
+            responses = {
+                    @ApiResponse(description = "No content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Error", responseCode = "500", content = @Content),
+            }
+    )
     public ResponseEntity<Object> deletePromotion(@PathVariable(value = "id") Long id){
         var promotion = promotionService.findById(id);
         try {
